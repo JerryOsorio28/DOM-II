f58f9b9dbf8c8c1eaa86106c22b681d5c8d81a18
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-07-09 20:00:55 -0400

    Added shake animation in index.js when holding down mouse button

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -7,8 +7,33 @@ function blastOff(){
     rockets[4].addEventListener('click', () => TweenMax.to("#grayRocket", 1, {y: -1000}));
 }
 
+function shake(){
+    rockets[0].addEventListener('mousedown', () => {
+        TweenMax.to("#redRocket", 0.1, {x:"+=20", yoyo:true, repeat:5}),
+        TweenMax.to("#redRocket", 0.1, {x:"-=20", yoyo:true, repeat:5})
+    });
+    rockets[1].addEventListener('mousedown', () => {
+        TweenMax.to("#blueRocket", 0.1, {x:"+=20", yoyo:true, repeat:5}),
+        TweenMax.to("#blueRocket", 0.1, {x:"-=20", yoyo:true, repeat:5})
+    });
+    rockets[2].addEventListener('mousedown', () => {
+        TweenMax.to("#greenRocket", 0.1, {x:"+=20", yoyo:true, repeat:5}),
+        TweenMax.to("#greenRocket", 0.1, {x:"-=20", yoyo:true, repeat:5})
+    });
+    rockets[3].addEventListener('mousedown', () => {
+        TweenMax.to("#pinkRocket", 0.1, {x:"+=20", yoyo:true, repeat:5}),
+        TweenMax.to("#pinkRocket", 0.1, {x:"-=20", yoyo:true, repeat:5})
+    });
+    rockets[4].addEventListener('mousedown', () => {
+        TweenMax.to("#grayRocket", 0.1, {x:"+=20", yoyo:true, repeat:5}),
+        TweenMax.to("#grayRocket", 0.1, {x:"-=20", yoyo:true, repeat:5})
+    });
+}
+
+//<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FUNCTIONS
 const rockets = document.querySelector('.blocks').children;
 
 blastOff();
+shake();
 
 
